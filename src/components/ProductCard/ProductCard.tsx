96352dd54665ca3c0153d5d86a706cb073c853fe
Vadym12122 <99530322+Vadym12122@users.noreply.github.com>
2024-03-04 16:47:21 +0200

    product_cart_done (#4)

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,32 +1,50 @@
 import { ProductCardProps } from "./ProductCard.props";
 import styles from "../ProductCard/ProductCard.module.css";
 import { Link } from "react-router-dom";
+import { MouseEvent } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../store/store";
+import { cartActions } from "../../store/cart.slice";
 
 function ProductCard(props: ProductCardProps) {
+    const dispatch = useDispatch<AppDispatch>();
+
+    const add = (e: MouseEvent) => {
+        e.preventDefault();
+        dispatch(cartActions.add(props.id));
+    };
+
     return (
-        <Link to={`/product/${props.id}`} className={styles['link']}>
-            <div className={styles['card']}>
-                <div className={styles['head']} style={{backgroundImage: `url('${props.image}')`}}>
-                <div className={styles['price']}>
-                    {props.price}&nbsp;
-                    <span className={styles['currency']}>грн</span>
+        <Link to={`/product/${props.id}`} className={styles["link"]}>
+            <div className={styles["card"]}>
+                <div
+                    className={styles["head"]}
+                    style={{ backgroundImage: `url('${props.image}')` }}
+                >
+                    <div className={styles["price"]}>
+                        {props.price}&nbsp;
+                        <span className={styles["currency"]}>грн</span>
+                    </div>
+                    <button className={styles["add-to-cart"]} onClick={add}>
+                        <img
+                            src="/cart-button-icon.svg"
+                            alt="Добавити в корзину"
+                        />
+                    </button>
+                    <div className={styles["rating"]}>
+                        {props.rating}&nbsp;
+                        <img src="/star-icon.svg" alt="Іконка зірки" />
+                    </div>
                 </div>
-                <button className={styles['add-to-cart']}>
-                    <img src="/cart-button-icon.svg" alt="Добавити в корзину"/>
-                </button>
-                <div className={styles['rating']}>
-                    {props.rating}&nbsp;
-                    <img src="/star-icon.svg" alt="Іконка зірки"/>
-                </div>
-                </div>
-                    <div className={styles['footer']}>
-                    <div className={styles['title']}>{props.name}</div>
-                    <div className={styles['description']}>{props.description}</div>
+                <div className={styles["footer"]}>
+                    <div className={styles["title"]}>{props.name}</div>
+                    <div className={styles["description"]}>
+                        {props.description}
+                    </div>
                 </div>
             </div>
         </Link>
-
-    )
+    );
 }
 
 export default ProductCard;
