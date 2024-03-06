82dddad70e572d9fd775bfb1ea8f352143647406
Vadym <vadimkotelets585@gmail.com>
2024-03-06 20:02:46 +0200

    Cart Component

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -56,8 +56,13 @@ export function Layout() {
                     >
                         <img src="/cart-icon.svg" alt="Cart" />
                         Корзина
+                        <span className={styles["cart-count"]}>
+                            {items.reduce(
+                                (acc, item) => (acc += item.count),
+                                0
+                            )}
+                        </span>
                     </NavLink>
-                    {items.reduce((acc, item) => (acc += item.count), 0)}
                 </div>
                 <Button className={styles["exit"]} onClick={logout}>
                     <img src="/exit-icon.svg" alt="Exit" />
